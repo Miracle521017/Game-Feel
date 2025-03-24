66343d0826729bb98776f72e2e436d8ecd9df30f
Sauuuu\长安 <2514580183@qq.com>
2025-03-24 09:50:42 +0800

    完善基本逻辑

--- Game-Feel/Assets/Scripts/Game/PlayerController.cs
+++ Game-Feel/Assets/Scripts/Game/PlayerController.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public AudioSource cut;
+
     public KeyCode left=KeyCode.A;
     public KeyCode right=KeyCode.D;
+
+    public Animator leftAni;
+    public Animator leftAni_1;
+    public Animator rightAni;
+    public Animator rightAni_1;
     void Update()
     {
-        if (Input.GetKeyDown(left))
+        CutCheck(left, leftAni, leftAni_1);
+        CutCheck(right, rightAni, rightAni_1);
+    }
+
+    void CutCheck(KeyCode keyCode,Animator animator,Animator animator1)
+    {
+        if (Input.GetKeyDown(keyCode))
         {
-            Debug.Log("左手切下！");
+            animator.SetBool("Cut", true);
+            animator1.SetBool("Cut", true);
+            cut.Play();
+
         }
-        if (Input.GetKeyDown(right))
+        if (Input.GetKeyUp(keyCode))
         {
-            Debug.Log("右手切下！");
+            animator.SetBool("Cut", false);
+            animator1.SetBool("Cut", false);
         }
     }
 }
